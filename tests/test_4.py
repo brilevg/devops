b04b89e3e542cc99d64f630efab7e1ed73eae11e
brilevg <92127061+brilevg@users.noreply.github.com>
2025-12-04 23:57:09 +1000

    Update test_4.py

--- tests/test_4.py
+++ tests/test_4.py
@@ -11,7 +11,7 @@ class TestDatabaseConnection(unittest.TestCase):
         cfg = {
             "host": os.environ.get("POSTGRES_HOST", "localhost"),
             "port": os.environ.get("POSTGRES_PORT", 5432),
-            "dbname": os.environ["POSTGRES_NAME"],
+            "dbname": os.environ["POSTGRES_DB"],
             "user": os.environ["POSTGRES_USER"],
             "password": os.environ["POSTGRES_PASSWORD"],
         }
