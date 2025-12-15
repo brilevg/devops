d037150647d186d5d00bec25d89bf4cba1bc45e6
brilevg <92127061+brilevg@users.noreply.github.com>
2025-12-15 16:18:05 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, 
-request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import os
 import time
 from datetime import datetime
